be9d994f1f647dad9a6961a116e4eb996ad01afb
Abhisheknayak-programer <sahiservice300@gmail.com>
2024-01-17 21:31:09 +0530

    Session 9 Done

--- Homework.cpp
+++ Homework.cpp
@@ -1,10 +1,3 @@
 // HOMEWORK QUESTIONS
-// 1. Print a pattern
-//              1
-//          1   2   1
-//       1  2   3   2  1
-//    1  2  3  4   3  2  1
-
-// 2. Given an array of integers return true if the numbers of the occurrences of each value in the array is unique or false
-// 3. Two arrays are given write a function to check same elements are present in two arrays or not.
-// 4. Do a XOR operation on the array which contains all same elements but one unique element.
+// 1. Swap Alternative Numbers present inside the array using functions
+// 2. Find the second largest element and second smallest element in the array and also use functions
--- Q28.cpp
+++ Q28.cpp
@@ -13,13 +13,29 @@ bool LinearSearch(int arr[], int size, int num)
     return false;
 }
 
+void LinearSearchInd(int arr[], int size, int num)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] == num)
+        {
+            cout << "Number Found at Index : " << i << endl;
+        }
+    }
+}
+
 int main()
 {
     int first[10] = {4, 5, 6, 7, 7, 8, 89, 2, 44, 404};
     int size = sizeof(first) / sizeof(int);
-    if(LinearSearch(first, size, 414)){
+    if (LinearSearch(first, size, 414))
+    {
         cout << "Number Found";
-    }else{
+    }
+    else
+    {
         cout << "Number Not Found";
     }
+
+    LinearSearchInd(first, size, 7);
 }
